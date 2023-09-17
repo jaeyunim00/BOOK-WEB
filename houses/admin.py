819dd0c78576831deaf2dff6d54d98e9087569eb
jaeyunim00 <71920791+jaeyunim00@users.noreply.github.com>
2023-09-17 16:01:22 +0900

    house done

--- houses/admin.py
+++ houses/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import House
 # Register your models here.
+
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    
+    list_display = [
+        "name",
+        "price_per_night",
+        "address",
+        "isBooked"
+    ]
+
+    list_filter = ["price_per_night", "isBooked"]
+
+    search_fields = ["address"]
+
+    list_display_links = ["name", "address"]
+
--- houses/models.py
+++ houses/models.py
@@ -3,8 +3,15 @@ from django.db import models
 # Create your models here.
 
 class House(models.Model):
-    
-    name = models.CharField(max_length=140)
-    price = models.PositiveBigIntegerField()
-    isBooked = models.BooleanField()
-    
+
+  """House Model"""
+
+  name = models.CharField(max_length=140)
+  price_per_night = models.PositiveBigIntegerField()
+  description = models.TextField()
+  isBooked = models.BooleanField(default=True)
+  address = models.CharField(max_length=140)
+
+  def __str__(self):
+    return self.name
+
